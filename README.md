4a7ca755c33c016a64f6f94830d1b818eb3502d3
Samuel Gregson <55996456+Zacxalot@users.noreply.github.com>
2023-12-05 18:58:30 +0000

    Day 2 Part 2 Kinda quick

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Advent of Code solutions for 2023!
 
 | Day | Part 1 | Part 2 | Duration | No IO Duration |
 | --- | ------ | ------ | -------- | -------------- |
-| 1   | 55208  | 54578  | 370μs    | 243μs          |
+| 1   | 55208  | 54578  | 370μs    | 251μs          |
+| 2   | 2600   | 86036  | 1940μs   | 1866μs         |
 
-Total duration: 370μs (Total no IO duration 243μs)
+Total duration: 2311μs (Total no IO duration 2117μs)
--- src/days/day_2.rs
+++ src/days/day_2.rs
@@ -1,12 +1,16 @@
-use itertools::Itertools;
+use regex::Regex;
 use std::{fs, time::Instant};
 
 use crate::Answer;
+
+static MATCHING: &str = r"((\d+) (\w+))";
 
 pub fn execute() -> Answer {
     let time_before = Instant::now();
     let file = fs::read_to_string("./input/day_2.txt").unwrap();
     let time_no_io = Instant::now();
+
+    let re = Regex::new(MATCHING).unwrap();
 
     let lines = file.lines();
 
@@ -14,21 +18,20 @@ pub fn execute() -> Answer {
     let max_green = 13;
     let max_blue = 14;
 
-    let total = lines
-        .map(parse_line)
-        .enumerate()
-        .fold(0, |acc, (index, val)| {
-            if val.red <= max_red && val.blue <= max_blue && val.green <= max_green {
-                println!("{:?} {:?}", index + 1, val);
+    let parsed = lines.map(|line| parse_line(line, &re));
 
-                acc + index + 1
-            } else {
-                acc
-            }
-        });
+    let total = parsed.clone().enumerate().fold(0, |acc, (index, val)| {
+        if val.red <= max_red && val.blue <= max_blue && val.green <= max_green {
+            acc + index + 1
+        } else {
+            acc
+        }
+    });
+
+    let powers: i32 = parsed.map(|val| val.red * val.blue * val.green).sum();
 
     let part_1 = total.to_string();
-    let part_2 = "2".to_string();
+    let part_2 = powers.to_string();
 
     let duration = Instant::now() - time_before;
     let no_io_duration = Instant::now() - time_no_io;
@@ -42,70 +45,31 @@ pub fn execute() -> Answer {
     }
 }
 
-enum Colour {
-    Red,
-    Blue,
-    Green,
-}
-
-fn parse_line(line: &str) -> RGB {
+fn parse_line(line: &str, re: &Regex) -> Rgb {
     let mut red = 0;
     let mut green = 0;
     let mut blue = 0;
 
-    line.split(": ")
-        .nth(1)
-        .unwrap()
-        .split(';')
-        .for_each(|pull| {
-            let mut pull_red = 0;
-            let mut pull_blue = 0;
-            let mut pull_green = 0;
+    re.captures_iter(line).for_each(|re_match| {
+        let colour = &re_match[3];
+        let value = re_match[2].parse::<i32>().unwrap();
 
-            pull.split(',').for_each(|pair| {
-                let mut split = pair.trim().split(' ');
-                let val = split.next().unwrap();
-                println!("PAIR {pair}");
-                // println!("Val {val} colour - {:?}", split.next().unwrap());
-                let colour = match_colour(split.next().unwrap());
+        match colour {
+            "red" => red = red.max(value),
+            "green" => green = green.max(value),
+            "blue" => blue = blue.max(value),
+            _ => (),
+        }
+    });
 
-                match colour {
-                    Colour::Red => pull_red += val.parse::<i32>().unwrap(),
-                    Colour::Blue => pull_blue += val.parse::<i32>().unwrap(),
-                    Colour::Green => pull_green += val.parse::<i32>().unwrap(),
-                }
-            });
-
-            println!("LINE ---- {pull}");
-
-            red = red.max(pull_red);
-            green = green.max(pull_green);
-            blue = blue.max(pull_blue);
-        });
-
-    RGB { red, blue, green }
+    Rgb { red, blue, green }
 }
 
 #[derive(Debug)]
-struct RGB {
+struct Rgb {
     red: i32,
     blue: i32,
     green: i32,
-}
-
-fn match_colour(colour: &str) -> Colour {
-    match colour {
-        "red" => Colour::Red,
-        "blue" => Colour::Blue,
-        "green" => Colour::Green,
-        "red;" => Colour::Red,
-        "blue;" => Colour::Blue,
-        "green;" => Colour::Green,
-        "red," => Colour::Red,
-        "blue," => Colour::Blue,
-        "green," => Colour::Green,
-        _ => panic!("Invalid colour"),
-    }
 }
 
 #[cfg(test)]
@@ -114,10 +78,9 @@ mod tests {
 
     #[test]
     fn test_parse_line() {
+        let re = Regex::new(MATCHING).unwrap();
         let line = "Game 3: 12 red, 18 blue, 3 green; 14 red, 4 blue, 2 green; 4 green, 15 red";
-        let rgb = parse_line(line);
-
-        println!("{:?}", rgb);
+        let rgb = parse_line(line, &re);
 
         assert_eq!(rgb.red, 15);
         assert_eq!(rgb.green, 4);
